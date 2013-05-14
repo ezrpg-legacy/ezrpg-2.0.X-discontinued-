3eb6b886ce843777fcf8300ddf817607381b1b86
Tim Garrity <timgarrity89@gmail.com>
2013-05-14 03:13:50 -0400

    Removed Example script | Replaced with Login info
    Swiftlet started us out with example code, that was removed.
    Added a check based on session to see if userID is set, see
    exRPG/Models/Session.
    Created a "loggedIn" variable to mimic the old "LOGGED_IN" constant in
    templates.
    Made http://ezrpg/index serve up either index.phtml OR home.phtml based
    on if user is logged in or not.
    Created a "player" variable for the 'home' view to access Player's stats
    and info.

--- ezRPG/Controllers/Index.php
+++ ezRPG/Controllers/Index.php
@@ -12,15 +12,15 @@ class Index extends \ezRPG\Controller
 	 */
 	public function index()
 	{
-		// Some example code to get you started
-
-		// Create a model instance, see /ezRPG/Models/Example.php
-		$exampleModel = $this->app->getModel('example');
-
-		// Get some data from the model
-		$helloWorld = $exampleModel->getHelloWorld();
-
-		// Pass the data to the view to display it
-		$this->view->set('helloWorld', $helloWorld);
+		$checkUser = $this->app->getSingleton('session')->loggedIn();
+		$this->view->set('loggedIn', $checkUser);
+		if ($checkUser === false ){
+			$this->view->name = 'index';
+		} else {
+			$userID = $this->app->getSingleton('session')->get('userid');
+			$this->view->name = 'home';
+			$this->view->set('player', $this->app->getSingleton('auth')->getUser($userID));
+		}
 	}
 }
+ 
