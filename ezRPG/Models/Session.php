55643d45a3646cf2546a094eaeae234cac4d82b5
Tim Garrity <timgarrity89@gmail.com>
2013-05-14 03:26:53 -0400

    Added a number of functions to comply with Rework
    Added $messages but not in use yet
    
    Ported over the generateSigature, CompareSignature, and setMessage from
    Rework.2b
    
    Created a validUser and loggedIn function.
    validUser can be used for pages that require members to be logged in.
    Unauthorized visitors are redirected to index.
    
    loggedIn is used to return a bool if the session key 'userid' is set

--- ezRPG/Models/Session.php
+++ ezRPG/Models/Session.php
@@ -4,11 +4,19 @@ namespace ezRPG\Models;
 
 class Session extends \ezRPG\Model
 {
+	protected $messages = array(
+		'INFO'		=> '',
+		'WARN'		=> '',
+		'FAIL'		=> '',
+		'GOOD'		=> ''
+	);
+	
 	public function __construct(\ezRPG\Interfaces\App $app)
 	{
 		parent::__construct($app);
-
+		
 		session_start();
+		
 	}
 
 	/**
@@ -39,5 +47,67 @@ class Session extends \ezRPG\Model
 	public function clear()
 	{
 		$_SESSION = array();
+	}
+	
+	function generateSignature() 
+	{
+		
+		$client = array_key_exists('userid', $_SESSION) ? 
+						$_SESSION['userid'] : 'guest';
+		
+		if ( $client == 'guest' )
+			$key = $this->app->getSingleton('auth')->generateSalt;
+		else
+			$key = $this->app->getSingleton('auth')->getUser($client)->secret_key;
+			
+		$bits = array(
+			'userid'    => $client,
+			'ip'        => $_SERVER['REMOTE_ADDR'],
+			'browser'   => $_SERVER['HTTP_USER_AGENT'],
+			'key'       => $key
+		);
+			
+		$signature = false;
+
+		foreach($bits as $key => $bit) {
+			$signature .= $key . $bit; 
+		}    
+		
+		return sha1($signature);
+	}
+
+	function compareSignature($origin) {
+		return $origin === generateSignature();
+	}
+	
+	public function setMessage($message, $level='info') {
+		$level = strtoupper($level);
+		
+		// for better practices.
+		if (array_key_exists($level, $this->messages) === false) {
+			throw new \Exception('Message level "' . $level . '" does not exists.');
+			return false;
+		}
+		
+		$this->messages[$level] .= $message;
+		return true;
+	}
+	public function validUser(){
+		if ( !( $playerId = $this->get('userid') ) ) {
+			header('HTTP/1.0 403 Forbidden');
+
+			header('Location: index');
+
+			exit;
+		}
+
+		return $playerId;
+	}
+	
+	public function loggedIn(){
+		if (  $this->get('userid')  ) {
+			return true;
+		}
+		return false;
 	}
 }
