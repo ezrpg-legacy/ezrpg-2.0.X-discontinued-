c97876faafe63eb36418374e91392fa9505d43ad
uaktags <timgarrity89@gmail.com>
2013-05-29 14:09:09 -0300

    Index module

---
+++ Module/Index/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ezRPG\Module\Index;
+use ezRPG\Library\Module;
+
+class Index extends Module
+{
+    public function index() {
+      $this->view->name = 'index';
+    }
+	public function hello() {
+
+	}
+}
