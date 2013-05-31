483f7b9d6c17a6f6776c4e147f2926e67e46e2cf
uaktags <timgarrity89@gmail.com>
2013-05-31 08:40:57 -0300

    Ported PDO Model from 'master'

---
+++ Library/Model/Pdo.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ezRPG\Library\Model;
+use \ezRPG\Library\Model;
+
+class Pdo extends Model
+{
+  protected
+		$handle
+		;
+
+	/**
+	 * Establish database connection
+	 * @param object $app
+	 */
+	public function __construct(\ezRPG\Library\Interfaces\App $app)
+	{
+			parent::__construct($app);
+
+		//require('config/pdo.php');
+
+		$config = $this->app->getConfig('db');
+
+		try {
+			$this->handle = new \PDO($config['driver'] . ':host=' . $config['host'] . ';port='. $config['port'] . ';dbname=' . $config['database'].';charset=utf8', $config['username'], $config['password']);
+		} catch ( \PDOException $e ) {
+			throw new \Exception('Error establishing database connection: ' . $e->getMessage());
+		}
+
+		$this->handle->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+		$this->handle->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_OBJ);
+	}
+
+	public function getHandle()
+	{
+		return $this->handle;
+	}
+}
